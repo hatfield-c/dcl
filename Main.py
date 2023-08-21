983944fac68505a544f1a512b011758d69454283
Cody Hatfield <hatfield.coders@gmail.com>
2023-08-21 11:44:08 -0500

    [DCL-4] Simple hover drone implemented in pybullet

--- Main.py
+++ Main.py
@@ -1,24 +1,60 @@
-import pybullet as p
+import pybullet as pb
 import time
 import pybullet_data
 
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,0)#-10)
+physicsClient = pb.connect(pb.GUI)#or pb.DIRECT for non-graphical version
+pb.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+pb.setGravity(0,0,-10)
 
-planeId = p.loadURDF("plane.urdf")
+planeId = pb.loadURDF("plane.urdf")
 
 startPos = [0,0,1]
-startOrientation = p.getQuaternionFromEuler([0,0,0])
+startOrientation = pb.getQuaternionFromEuler([0,0,0])
 
-boxId = p.loadURDF("test_agent.urdf",startPos, startOrientation)
+drone_id = pb.loadURDF("drone_simple.urdf",startPos, startOrientation)
 
-#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+force0 = 0.5
+
+#links 7 to 10
 for i in range (10000):
-    p.stepSimulation()
-    time.sleep(1./240.)
 	
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+	if i > 85:
+		pb.applyExternalForce(
+			drone_id,
+			0,
+			forceObj=[0, 0, force0],
+			posObj=[0, 0, 0],
+			flags=pb.LINK_FRAME
+		)
+		pb.applyExternalForce(
+			drone_id,
+			1,
+			forceObj=[0, 0, force0],
+			posObj=[0, 0, 0],
+			flags=pb.LINK_FRAME
+		)
+		pb.applyExternalForce(
+			drone_id,
+			2,
+			forceObj=[0, 0, force0],
+			posObj=[0, 0, 0],
+			flags=pb.LINK_FRAME
+		)
+		pb.applyExternalForce(
+			drone_id,
+			3,
+			forceObj=[0, 0, force0],
+			posObj=[0, 0, 0],
+			flags=pb.LINK_FRAME
+		)
+	
+	if i > 100:
+		force0 = 0.067
+	
+	pb.stepSimulation()
+	time.sleep(1./240.)
+	
+cubePos, cubeOrn = pb.getBasePositionAndOrientation(drone_id)
 print(cubePos,cubeOrn)
 
-p.disconnect()
+pb.disconnect()
