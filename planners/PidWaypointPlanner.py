1f6a3b8847e08d946611b8e17218e970fc469241
Cody Hatfield <hatfield.coders@gmail.com>
2023-09-10 18:12:38 -0500

    [DCL-13] Work began on hover action

--- planners/PidWaypointPlanner.py
+++ planners/PidWaypointPlanner.py
@@ -5,14 +5,20 @@ import planners.PlannerInterface as PlannerInterface
 
 class PidWaypointPlanner(PlannerInterface.PlannerInterface):
 	
-	def __init__(self, waypoints, waypoint_threshold = 0.5):
+	def __init__(self, waypoints, waypoint_threshold = 0.5, alignment_threshold = 0.5):
 		self.waypoints = waypoints
 		self.waypoint_threshold = waypoint_threshold
+		self.alignment_threshold = alignment_threshold
 		
-		self.forward = np.array([0, 1, 0])
+		self.HOVER_ACTION = "hover"
+		self.MOVE_ACTION = "move"
+		
+		self.current_action = "hover"
+		
 	
 	def GetPlan(self, sensors, metadata):
 		gps = sensors["gps"]
+		gyro = sensors["gyro"]
 		quat = sensors["quat"]
 		velocity_sensor = sensors["velocity"]
 		
@@ -26,12 +32,17 @@ class PidWaypointPlanner(PlannerInterface.PlannerInterface):
 			distance = 1
 		
 		desired_direction = diff / distance
-		
-		velocity = velocity_sensor.ReadSensor(None)
 
+		current_rotation = gyro.ReadSensor(None)
+		current_direction = self.RotationToDirection(current_rotation)
+
+		velocity = velocity_sensor.ReadSensor(None)
 		current_quat = quat.ReadSensor(None)
 
+		action = self.ChooseAction(current_position, current_direction, desired_direction)
+		
 		plan = {
+			"action": action,
 			"current_quat": current_quat,
 			"current_altitude": current_position[2],
 			"desired_direction": desired_direction, 
@@ -41,16 +52,43 @@ class PidWaypointPlanner(PlannerInterface.PlannerInterface):
 		
 		return plan
 			
+	def ChooseAction(self, current_position, current_direction, target_direction):
+		return self.MOVE_ACTION
+		
+		target_distance = self.GetTargetDistance(current_position)
+		alignment = np.dot(current_direction, target_direction)
+		
+		if target_distance > self.waypoint_threshold and alignment > self.alignment_threshold:
+			return self.MOVE_ACTION
+		
+		return self.HOVER_ACTION
+	
+	def GetTargetDistance(self, current_position):
+		target_position = self.waypoints[0]
+		diff = target_position - current_position
+		distance = np.linalg.norm(diff)
+		
+		return distance
+	
 	def GetWaypoint(self, current_position):
 		
 		while(len(self.waypoints) > 0):
 			next_position = self.waypoints[0]
-			diff = next_position - current_position
-			distance = np.linalg.norm(diff)
+			distance = self.GetTargetDistance(current_position)
 			
 			if distance > self.waypoint_threshold:
 				return next_position
 			
 			self.waypoints.pop(0)
-				
+			
 		return current_position
+	
+	def RotationToDirection(self, rotation):
+		x = -np.sin(rotation[2]) * np.cos(rotation[0])
+		y = np.cos(rotation[2]) * np.cos(rotation[0])
+		z = np.sin(rotation[0])
+		
+		direction = np.array([x, y, z])
+		magnitude = np.linalg.norm(direction)
+		
+		return direction / magnitude
