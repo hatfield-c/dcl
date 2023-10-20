877ec3a5fda1ae99d454fd8495694f9b8bc2cdf6
Cody Hatfield <hatfield.coders@gmail.com>
2023-10-19 23:33:38 -0500

    [DCL-59] Rudimentary rotor strength is now limited

--- actuators/RotorActuator.py
+++ actuators/RotorActuator.py
@@ -4,8 +4,9 @@ import pybullet as pb
 import actuators.ActuatorInterface as ActuatorInterface
 
 class RotorActuator(ActuatorInterface.ActuatorInterface):
-	def __init__(self):
+	def __init__(self, rotor_max = 0.25):
 		self.last_command = None
+		self.rotor_max = rotor_max
 
 	def Actuate(self, control_data):
 
@@ -16,31 +17,42 @@ class RotorActuator(ActuatorInterface.ActuatorInterface):
 			control_data["yaw_signal"],
 		])
 
+		fr_rotor = control_data["fr_rotor_force"]
+		fl_rotor = control_data["fl_rotor_force"]
+		br_rotor = control_data["br_rotor_force"]
+		bl_rotor = control_data["bl_rotor_force"]
+		torque = control_data["torque"]
+
+		fr_rotor = np.clip(fr_rotor, 0, self.rotor_max)
+		fl_rotor = np.clip(fl_rotor, 0, self.rotor_max)
+		br_rotor = np.clip(br_rotor, 0, self.rotor_max)
+		bl_rotor = np.clip(bl_rotor, 0, self.rotor_max)
+
 		pb.applyExternalForce(
 			control_data["pb_id"],
 			0,
-			forceObj = [0, 0, control_data["fr_rotor_force"]],
+			forceObj = [0, 0, fr_rotor],
 			posObj = [0, 0, 0],
 			flags = pb.LINK_FRAME
 		)
 		pb.applyExternalForce(
 			control_data["pb_id"],
 			1,
-			forceObj = [0, 0, control_data["fl_rotor_force"]],
+			forceObj = [0, 0, fl_rotor],
 			posObj = [0, 0, 0],
 			flags = pb.LINK_FRAME
 		)
 		pb.applyExternalForce(
 			control_data["pb_id"],
 			2,
-			forceObj = [0, 0, control_data["br_rotor_force"]],
+			forceObj = [0, 0, br_rotor],
 			posObj = [0, 0, 0],
 			flags = pb.LINK_FRAME
 		)
 		pb.applyExternalForce(
 			control_data["pb_id"],
 			3,
-			forceObj = [0, 0, control_data["bl_rotor_force"]],
+			forceObj = [0, 0, bl_rotor],
 			posObj = [0, 0, 0],
 			flags = pb.LINK_FRAME
 		)
@@ -48,7 +60,7 @@ class RotorActuator(ActuatorInterface.ActuatorInterface):
 		pb.applyExternalTorque(
 			control_data["pb_id"],
 			-1,
-			torqueObj = [0, 0, control_data["torque"]],
+			torqueObj = [0, 0, torque],
 			flags = pb.LINK_FRAME
 		)
 
